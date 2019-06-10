87e0c9f2b2045683817086acb02a450d1f445049
cooperb21 <cooperbossert@yahoo.com>
2019-06-09 22:05:36 -0500

    Persisting Data Using Core Data

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 //Since we inherited UITableViewController, it is main class not subclass. So we don't need to add delegates, datasource, hook up IBOutlets, it's automated by Xcode
 class TodoListViewController: UITableViewController {
     
@@ -15,14 +16,19 @@ class TodoListViewController: UITableViewController {
     //NSCoder converts array of items into a plist that we can save and retreive from
     var itemArray = [Item]()
     
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    var selectedCategory : Category? {
+        didSet {
+            loadItems()
+        }
+    }
+    
+    //Tapping into UIApplication class, getting shared singleton object which corrosponds to the current app as an object, tapping into it's delegate, casting it into our class AppDelegate. We now has access to our app delegate as an object, and can tap into it's property called persistentContainer, and grab the viewContext of that
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(dataFilePath)
-        
-        loadItems()
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
         // Do any additional setup after loading the view.
         //Load prior data
@@ -57,10 +63,14 @@ class TodoListViewController: UITableViewController {
     
     //Tells the specified row that it is selected
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
+        
+        //        //Removing the data from our permanent stores
+        //        context.delete(itemArray[indexPath.row])
+        //        //Removing current item from item array which is used to load the tableView datasource
+        //        itemArray.remove(at: indexPath.row)
         
         //Give selected cell a checkmark as an accessory
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        //itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         saveItems()
         
@@ -76,9 +86,12 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
             //What will happen once the user clicks the 'Add Item' button on our UIAlert
-            let newItem = Item()
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             //Save new itemArray
             self.saveItems()
@@ -96,27 +109,74 @@ class TodoListViewController: UITableViewController {
     }
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("Error encoding item array, \(error)")
+            print("Error saving context \(error)")
         }
         //Reload tableview data
-        tableView.reloadData()
+        self.tableView.reloadData()
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Error decoding item array, \(error)")
-            }
+    // = Item.fetchRequest() is default value if no arguements passed in when calling
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
         }
+        
+//        let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, predicate])
+//
+//        request.predicate = compoundPredicate
+        
+        //Fetch request, which is everything inside our persistent container
+        do {
+            //This output will be an array of items inside our persistent container
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
     }
     
 }
 
+//MARK: - Search bar methods
+
+//Advantage of this is that we can split up the functionality of our view controller to have specific parts be responsible for specific things
+//Modular design
+//Better organized code + easier to debug + MARKS section
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        //When we hit search button, whatever is entered in the search bar will replace the %@ below
+        //[cd] will make the search feature non case sensitive and non diacritic
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        //Sort by title in alphabetical order
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request, predicate: predicate)
+        
+    }
+    
+    //Reset view after search
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //If text in searchbar has changed but gone to zero
+        //Not called on open because there was no change
+        if searchBar.text?.count == 0 {
+            loadItems()
+            //Assign projects to different threads
+            DispatchQueue.main.async {
+                //Relinquish status as first responder
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+}
